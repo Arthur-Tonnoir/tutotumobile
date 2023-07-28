2d6e569962049d2ab43edae8f5a534c4dc290597
Arthur Tonnoir <arthur1.tonnoir@sfr.fr>
2023-07-28 16:50:52 +0200

    feat: add new routes, louer and contact :sparkles:

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Acceuil from '../Pages/Accueil/Acceuil';
 import NavBar from '../Components/NavBar/NavBar';
+import VoitureLoue from '../Pages/VoitureLoue/VoitureLoue';
+import Contact from '../Pages/Contact/Contact';
 
 function Router(){
     return (
@@ -9,6 +11,8 @@ function Router(){
             <NavBar/>
             <Routes>
                 <Route path='/' element={<Acceuil/>}/>
+                <Route path='/louer' element={<VoitureLoue/>}/>
+                <Route path='/contact' element={<Contact/>}/>
             </Routes>
         </div>
     )
